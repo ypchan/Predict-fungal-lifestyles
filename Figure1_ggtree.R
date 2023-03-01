003beb06c65aa612fc4e22963fce9c6f9b7ee995
Yanpeng Chen <chenyanpeng1992@outlook.com>
2023-03-01 14:00:49 +0800

    Update Figure1_ggtree.R

--- Figure1_ggtree.R
+++ Figure1_ggtree.R
@@ -1,22 +1,32 @@
+# set working directory
 setwd('G:\\Experiments\\Paper\\FD_lifestyle_changes\\11_stat')
+
+# list content in the directory
 dir()
 
+# load required packages
 library(treedataverse)
 library(tidyverse)
 library(readxl)
 library(ggstar)
 library(ggnewscale)
 
+# read in the ML tree
 tre <- read.iqtree('G:\\Experiments\\Paper\\FD_lifestyle_changes\\12_ml_tree\\640_1124_iqtree.treefile')
 
+# read in the meadadata table
 df <- read_excel('G:\\Experiments\\Paper\\FD_lifestyle_changes\\ncbi_genome_table_20220812\\20221107.xlsx')
+
+# convert the sequencing technologies into factor variables
 df$WGS_class <- factor(df$WGS_class)
 
-#geom_highlight
+# to label clades
 highlight_node_lst <- c(647,1010,1051,1116,1157,1167,1205,1226,1243,1229)
 node_label <- c('Hypocreales','Microascales', 'Glomerellales','Ophiostomatales','Magnaporthales',
                 'Diaporthales', 'Sordariales', 'Coniochaetales','Xylariales','Amphisphaeriales')
 nodedf <- data.frame(node=highlight_node_lst,label=node_label)
+
+# tree + calde labels
 p <- ggtree(tre, layout = 'fan',size=0.15) +
     geom_cladelab(data=nodedf, 
                   mapping=aes(node=node, 
@@ -24,6 +34,7 @@ p <- ggtree(tre, layout = 'fan',size=0.15) +
 p
 
 
+# terminal shapes represent the lifestyles
 p1 <- p + geom_fruit(data = df, geom = geom_star,
                mapping = aes(y=Assembly, fill=Lifestyle, starshape=WGS_class),
                position='identity',
@@ -39,6 +50,7 @@ df %>% select(Assembly,Gender) %>%
     gather(key='Gender', value = 'Values', -Assembly) -> gender_df
 typeof(gender_df$Values)
 
+### we already removethe morph information frome the figure
 p2 <- p1 + geom_fruit(data = gender_df,geom = geom_tile,
                 mapping = aes(y = Assembly, x = Gender, alpha=Values),
                 pwidth=0.02, color = "grey50", offset = 0.04,size = 0.02, fill='steelblue')  + 
